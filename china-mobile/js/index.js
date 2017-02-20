ae6d041949ac988244c36584f5c348ab678d2ccd
roundDB <1643453298@qq.com>
2017-02-21 00:45:13 +0800

    prettify nav-submenu

--- china-mobile/js/index.js
+++ china-mobile/js/index.js
@@ -3,20 +3,27 @@
  */
 (function(){
 	var oMenuLi=document.querySelectorAll('.menu-list');
+  var oSubMenu=document.querySelectorAll('.submenu-list');
 	var len=oMenuLi.length;
+  var timer=null;
 	for(let i=1;i<len;i++){
+
 		oMenuLi[i].onmouseover=function(){
 			var oUl=this.querySelectorAll("ul")[0];
 			var oIns=this.querySelectorAll("ins")[0];
 			oUl.style.left=(-260)*(i-1)-161+"px";
 			oUl.style.display='flex';
 			oIns.style.display='block';
-		}
+      }
+
 		oMenuLi[i].onmouseleave=function(){
-			var oUl=this.querySelectorAll("ul")[0];
-			var oIns=this.querySelectorAll("ins")[0];
-			oUl.style.display='none';
-			oIns.style.display='none';
+      var oUl=this.querySelectorAll("ul")[0];
+      var oIns=this.querySelectorAll("ins")[0];
+      // 二级菜单延时消失
+      timer=setTimeout(function(){
+        oUl.style.display='none';
+        oIns.style.display='none';
+      },300)
 		}
 	}
 	function showcarousal(){
